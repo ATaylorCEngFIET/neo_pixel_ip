ab31f0a95f325ef8fd97aa45c18182b5271e2212
Adam Taylor <adam@adiuvoengineering.com>
2023-07-10 13:07:10 +0100

    updated

--- neo.vhd
+++ neo.vhd
@@ -1,8 +1,33 @@
+-- Copyright (c) 2023, Adiuvo Engineering & Training Ltd
+-- All rights reserved.
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+-- 1. Redistributions of source code must retain the above copyright notice, this
+--    list of conditions and the following disclaimer. 
+-- 2. Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+-- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+-- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+-- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+-- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+-- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-- The views and conclusions contained in the software and documentation are those
+-- of the authors and should not be interpreted as representing official policies, 
+-- either expressed or implied, of the FreeBSD Project*/
+
+
 LIBRARY IEEE;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
+
 ENTITY neo_pixel IS PORT(
- clk   : IN std_logic;
+ clk   : IN std_logic; -- must be 20 MHz
  dout  : OUT std_logic;
  rstb  : OUT STD_LOGIC;
  enb   : OUT STD_LOGIC;
@@ -12,13 +37,16 @@ ENTITY neo_pixel IS PORT(
  doutb : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
 );
 END ENTITY;
+
 ARCHITECTURE rtl OF neo_pixel IS 
+
 TYPE FSM IS (idle,wait1,led,count,reset,addr_out,wait2,grab,wait_done,done_addr); 
 CONSTANT done  : std_logic_vector(25 DOWNTO 0) := "00000000000000000000000001";  --shows when shift reg empty
 CONSTANT zero  : std_logic_vector(24 DOWNTO 0) := "1111111000000000000000000"; --waveform for a zero bit
 CONSTANT one   : std_logic_vector(24 DOWNTO 0) := "1111111111111100000000000"; --waveform for a one bit 
 CONSTANT numb_pixels : integer := 24; --number of bits in a pixel
 CONSTANT reset_duration : integer := 1000;--number cclocks in the reset period
+
 SIGNAL shift_reg : std_logic_vector(24 DOWNTO 0) := (OTHERS=>'0'); -- shift reg containing the output pixel waveform
 SIGNAL shift_dne : std_logic_vector(25 DOWNTO 0) := (OTHERS=>'0'); -- shift reg for timing the output shift reg for next load
 SIGNAL current_state : fsm := idle; --fsm to control the pixel beign output 
@@ -31,9 +59,11 @@ SIGNAL ram_addr : integer RANGE 0 TO 1023:=0; --address to read from RAM
 SIGNAL led_cnt  : integer RANGE 0 TO 1023;--counts leds it has addressed 
 SIGNAL pixel    : std_logic_vector(23 DOWNTO 0); --holds led value to be output
 BEGIN
+
 web  <= (OTHERS => '0');
 rstb <=  '0';
 dinb <= (OTHERS =>'0');
+
 pixel_cntrl :  PROCESS(clk)
 BEGIN
  IF rising_edge(CLK) THEN 
@@ -103,6 +133,7 @@ BEGIN
    END CASE;
  END IF;
 END PROCESS;
+
 shr_op : PROCESS(clk)
 BEGIN
  IF rising_edge(clk) THEN 
@@ -119,5 +150,7 @@ BEGIN
    END IF;
  END IF;
 END PROCESS;
-dout <= shift_reg(shift_reg'high);     
+
+dout <= shift_reg(shift_reg'high); 
+
 END ARCHITECTURE;
